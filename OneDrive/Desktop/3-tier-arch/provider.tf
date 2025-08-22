ebe52c671da1a5020bd1a986dda13267d5cd7d19
Narendark790 <koletinarendar7@gmail.com>
2025-08-22 15:50:18 +0530

    Update provider.tf

--- OneDrive/Desktop/3-tier-arch/provider.tf
+++ OneDrive/Desktop/3-tier-arch/provider.tf
@@ -10,11 +10,3 @@ terraform {
 provider "aws" {
   region = "us-south-1"
 }
-
-resource "aws_s3_bucket" "example" {
-  bucket = "narendar-terraform-test-bucket"
-}
-
-output "bucket_name" {
-  value = aws_s3_bucket.example.bucket
-}
