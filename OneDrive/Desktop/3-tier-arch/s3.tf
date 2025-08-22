947b93a22114a6edfb2b92d08983486ca5398cf0
Narendark790 <koletinarendar7@gmail.com>
2025-08-22 15:45:56 +0530

    Update s3.tf

--- OneDrive/Desktop/3-tier-arch/s3.tf
+++ OneDrive/Desktop/3-tier-arch/s3.tf
@@ -1,10 +1,7 @@
-# Create an S3 Bucket
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swiggy-bucket-narendar"
-
-  # Enable versioning
-  versioning {
-    enabled = true
-  }
+resource "aws_s3_bucket" "example" {
+  bucket = "narendar-terraform-test-bucket"
 }
 
+output "bucket_name" {
+  value = aws_s3_bucket.example.bucket
+}
