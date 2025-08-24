dc47a6d2b5e4363a5880a81d789679fbe640cc46
Narendark790 <koletinarendar7@gmail.com>
2025-08-24 14:15:10 +0530

    Update sg.tf

--- OneDrive/Desktop/3-tier-arch/sg.tf
+++ OneDrive/Desktop/3-tier-arch/sg.tf
@@ -101,10 +101,11 @@ resource "aws_route_table_association" "pvt2" {
 }
 
 # Security group for EC2/ASG
+resource "aws_security_group" "app_sg" {
   name        = "swiggy-app-sg"
   description = "Allow traffic from ALB"
   vpc_id      = "vpc-0ab9b74d8c4bc3af0"
-
+}
 # App SG (allow 80 from ALB SG)
 resource "aws_security_group" "app_sg" {
   name   = "swiggy-app-sg"
