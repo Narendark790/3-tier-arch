b57bb58461566329ee9fa67866c78cc9e23ebf83
Narendark790 <koletinarendar7@gmail.com>
2025-08-24 13:16:49 +0530

    Update applb.tf

--- OneDrive/Desktop/3-tier-arch/applb.tf
+++ OneDrive/Desktop/3-tier-arch/applb.tf
@@ -1,11 +1,10 @@
-# Application Load Balancer
+# ALB
 resource "aws_lb" "app_lb" {
   name               = "swiggy-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [subnet-03fa54a742101e609, subnet-092bbebd2a91c7046]
-  tags               = { Name = "swiggy-alb" }
+  subnets            = ["subnet-03fa54a742101e609", "subnet-092bbebd2a91c7046"]
 }
 
 # Target Group
@@ -13,18 +12,12 @@ resource "aws_lb_target_group" "app_tg" {
   name     = "swiggy-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = vpc-0ab9b74d8c4bc3af0
+  vpc_id   = "vpc-0ab9b74d8c4bc3af0"
 
   health_check {
-    path                = "/"
-    matcher             = "200-399"
-    interval            = 30
-    unhealthy_threshold = 2
-    healthy_threshold   = 2
-    timeout             = 5
+    path    = "/"
+    matcher = "200-399"
   }
-
-  tags = { Name = "swiggy-tg" }
 }
 
 # Listener
@@ -37,4 +30,9 @@ resource "aws_lb_listener" "app_listener" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app_tg.arn
   }
+}
+
+# Output
+output "alb_dns_name" {
+  value = aws_lb.app_lb.dns_name
 }
