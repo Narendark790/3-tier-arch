8cdc3af2bcf4898c354b62585364711d84ad4512
Narendark790 <koletinarendar7@gmail.com>
2025-08-24 12:22:59 +0530

    Update appasg.tf

--- OneDrive/Desktop/3-tier-arch/appasg.tf
+++ OneDrive/Desktop/3-tier-arch/appasg.tf
@@ -1,21 +1,22 @@
 # Create an EC2 Auto Scaling Group - app
-resource "aws_autoscaling_group" "swiggy-app-asg" {
+resource "aws_autoscaling_group" "swiggy_app_asg" {
   name = "swiggy-app-asg"
 
   launch_template {
-    id      = aws_launch_template.swiggy-app-template.id
+    id      = aws_launch_template.swiggy_app_template.id
     version = "$Latest"
   }
 
   vpc_zone_identifier = [
-    aws_subnet.swiggy-pvt-sub-1.id,
-    aws_subnet.swiggy-pvt-sub-2.id
+    aws_subnet.swiggy_pvt_sub_1.id,
+    aws_subnet.swiggy_pvt_sub_2.id
   ]
 
   min_size         = 2
   max_size         = 3
   desired_capacity = 2
 }
+
 
 # Create a launch template for the EC2 instances
 resource "aws_launch_template" "swiggy-app-template" {
