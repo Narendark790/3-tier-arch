25409542c956cfdbed003a2d5d7cd4f7b59a706b
Narendark790 <koletinarendar7@gmail.com>
2025-08-24 12:36:15 +0530

    Update provider.tf

--- OneDrive/Desktop/3-tier-arch/provider.tf
+++ OneDrive/Desktop/3-tier-arch/provider.tf
@@ -1,12 +1,10 @@
 terraform {
+  required_version = ">= 1.3.0"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+    aws = { source = "hashicorp/aws", version = ">= 5.0" }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
