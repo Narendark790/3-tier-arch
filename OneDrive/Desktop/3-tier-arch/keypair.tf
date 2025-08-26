668e181cab86e6014ed11baa7fb7f5edf9fcaabd
Narendark790 <koletinarendar7@gmail.com>
2025-08-26 18:04:25 +0530

    Update keypair.tf

--- OneDrive/Desktop/3-tier-arch/keypair.tf
+++ OneDrive/Desktop/3-tier-arch/keypair.tf
@@ -1,10 +1,18 @@
-resource "tls_private_key" "rsa-4096-example" {
+# Key Pair
+
+# Create a new RSA private key
+resource "tls_private_key" "rsa" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "local_file" "private_key_pem" {
-  content  = tls_private_key.rsa-4096-example.private_key_pem
-  filename = "mumkey.pem"
+# Create a new local file to store the private key
+resource "local_file" "key" {
+  filename = "goutm.pem"
+  content  = tls_private_key.rsa.private_key_pem
 }
 
+resource "aws_key_pair" "kp" {
+  public_key = tls_private_key.rsa.public_key_openssh
+  key_name   = "goutm"
+}
