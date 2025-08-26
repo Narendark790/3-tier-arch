bdbad22dbddef22787c1a9410746af2e11080768
Narendark790 <koletinarendar7@gmail.com>
2025-08-26 18:00:46 +0530

    Update applb.tf

--- OneDrive/Desktop/3-tier-arch/applb.tf
+++ OneDrive/Desktop/3-tier-arch/applb.tf
@@ -1,38 +1,41 @@
-# ALB
-resource "aws_lb" "app_lb" {
-  name               = "swiggy-alb"
+# Application Load Balancer
+
+# Application Load Balancer Configuration
+resource "aws_lb" "app-lb" {
+  name               = "swigg-app-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = ["subnet-03fa54a742101e609", "subnet-092bbebd2a91c7046"]
+  security_groups    = [aws_security_group.ec2-lb.id]
+  subnets            = [aws_subnet.pub-subnet-1.id, aws_subnet.pub-subnet-2.id]
+
+  tags = {
+    "Name" = "swigg-app-lb"
+  }
+
 }
 
-# Target Group
-resource "aws_lb_target_group" "app_tg" {
-  name     = "swiggy-tg"
+# Application Load Balancer Target Group
+resource "aws_lb_target_group" "app-tg" {
+  name     = "swigg-app-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "vpc-0ab9b74d8c4bc3af0"
+  vpc_id   = aws_vpc.vpc.id
 
   health_check {
-    path    = "/"
-    matcher = "200-399"
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    "Name" = "swigg-app-tg"
   }
 }
 
-# Listener
-resource "aws_lb_listener" "app_listener" {
-  load_balancer_arn = aws_lb.app_lb.arn
-  port              = 80
-  protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
-  }
-}
-
-# Output
-output "alb_dns_name" {
-  value = aws_lb.app_lb.dns_name
+resource "aws_autoscaling_attachment" "app-asg-attachment" {
+  autoscaling_group_name = aws_autoscaling_group.web.name
+  lb_target_group_arn    = aws_lb_target_group.app-tg.arn
 }
