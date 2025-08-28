5206fbd16c69a508956b890af80d05d3f1bfb37a
Narendark790 <koletinarendar7@gmail.com>
2025-08-28 11:25:43 +0530

    Update db.tf

--- OneDrive/Desktop/3-tier-arch/db.tf
+++ OneDrive/Desktop/3-tier-arch/db.tf
@@ -7,8 +7,8 @@ resource "aws_db_subnet_group" "db" {
 }
 
 # MySQL RDS Instance
-resource "aws_db_instance" "data-instance" {
-  instance_class         = "db.t3.micro"
+resource "aws_data_instance" "data-instance" {
+  instance_class         = "data.t3.micro"
   allocated_storage      = 20
   storage_type           = "gp3"
   engine                 = "mysql"
