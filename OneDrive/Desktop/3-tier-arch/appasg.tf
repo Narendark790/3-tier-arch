6d60f47569fcf496ba3611e02948cc8e3a66984c
Narendark790 <koletinarendar7@gmail.com>
2025-08-28 21:32:43 +0530

    Update appasg.tf

--- OneDrive/Desktop/3-tier-arch/appasg.tf
+++ OneDrive/Desktop/3-tier-arch/appasg.tf
@@ -1,16 +1,25 @@
 # Application Auto Scaling Group
-
-# Create an Auto Scaling Group
 resource "aws_autoscaling_group" "app" {
   name = "swigg-app-asg"
+
   launch_template {
     id      = aws_launch_template.app-temp.id
     version = "$Latest"
   }
+
   max_size            = 4
   min_size            = 2
   desired_capacity    = 2
-  vpc_zone_identifier = [aws_subnet.priv-subnet-1.id, aws_subnet.priv-subnet-2.id]
+  vpc_zone_identifier = [
+    aws_subnet.priv-subnet-1.id,
+    aws_subnet.priv-subnet-2.id
+  ]
+
+  tag {
+    key                 = "Name"
+    value               = "swigg-app-asg"
+    propagate_at_launch = true
+  }
 }
 
 # Launch Template
@@ -18,23 +27,25 @@ resource "aws_launch_template" "app-temp" {
   name_prefix   = "app-launch-template-"
   image_id      = "ami-0861f4e788f5069dd"
   instance_type = "t3.micro"
-  key_name      = "nare"
 
+  key_name = "nare"
   network_interfaces {
     associate_public_ip_address = false
     security_groups             = [aws_security_group.ec2-app.id]
   }
 
   user_data = base64encode(<<-EOF
-  #!/bin/bash
-
-  sudo yum install mysql -y
-
+    #!/bin/bash
+    yum update -y
+    yum install mysql -y
+    systemctl enable mysql
+    systemctl start mysql
   EOF
   )
+
   lifecycle {
     prevent_destroy = false
-    ignore_changes  = all
+    ignore_changes  = [user_data]
   }
 
   tag_specifications {
@@ -43,5 +54,4 @@ resource "aws_launch_template" "app-temp" {
       Name = "app-launch-template-instance"
     }
   }
-
 }
